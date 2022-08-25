5fa960f03dc239cb967a2a3aea13fffaab9ad0fa
Min Maung <minmaung0307@gmail.com>
2022-08-25 18:17:38 -0400

    update some

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const { animals } = require("./data/animals");
 const express = require("express");
+const { animals } = require("./data/animals");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -28,11 +29,6 @@ const app = express();
 //   }
 //   return filteredResults;
 // }
-
-function findById(id, animalsArray) {
-  const result = animalsArray.filter((animal) => animal.id === id)[0];
-  return result;
-}
 
 //Modify filterByQuery
 function filterByQuery(query, animalsArray) {
@@ -80,23 +76,37 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 // app.get("/api/animals", (req, res) => {
 //   let results = animals;
 //   console.log(req.query);
 //   res.json(results);
 // });
 
-app.get("/api/animals", (req, res) => {
-  let results = animals;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
+// app.get("/api/animals", (req, res) => {
+//   let results = animals;
+//   if (req.query) {
+//     results = filterByQuery(req.query, results);
+//   }
+//   res.json(results);
+// });
+
+// app.get("/api/animals/:id", (req, res) => {
+//   const result = findById(req.params.id, animals);
+//   res.json(result);
+// });
 
 app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
-  res.json(result);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
 // app.listen(3001, () => {
